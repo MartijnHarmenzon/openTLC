febf2d3afddc3f37844212f7d77332abc8000c68
Martijn Harmenzon <martijn.harmenzon@maptm.nl>
2018-09-27 13:56:45 +0200

    Updated style to meet PEP guide

--- get_data.py
+++ get_data.py
@@ -1,22 +1,30 @@
+#! python3
+
+
 # import libraries
-import requests,yaml,time
+import requests
+import yaml
+import time
 
-# import settings
-with open('k0001appconf.yaml', 'r') as f:
-    appConfig = yaml.load(f)
 
-while True:
-    try:
-        if appConfig['automaat']['raspberry_pi']:
-            data = requests.get('http://192.168.1.36/data/data_from_gui.json')
-        else:
-            data = requests.get('http://localhost/MAP_K0001/data/data_from_gui.json')
+if __name__ == '__main__':
+    # import settings
+    with open('k0001appconf.yaml', 'r') as f:
+        appConfig = yaml.load(f)
 
-        if data.status_code == 200:
-            json_data = data.json()
-            yaml_data = open('data/data_from_gui.yaml', 'w+')
-            yaml.dump(json_data, yaml_data)
-    except:
-        pass
+    #
+    while True:
+        try:
+            if appConfig['automaat']['raspberry_pi']:
+                data = requests.get('http://192.168.1.36/data/data_from_gui.json')
+            else:
+                data = requests.get('http://localhost/MAP_K0001/data/data_from_gui.json')
 
-    time.sleep(0.1)
+            if data.status_code == 200:
+                json_data = data.json()
+                yaml_data = open('data/data_from_gui.yaml', 'w+')
+                yaml.dump(json_data, yaml_data)
+        except:
+            pass
+
+        time.sleep(0.1)
